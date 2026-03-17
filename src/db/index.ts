44debe2da0412ab853f8da7178563485b415eaf3
Ebin jac <ebinjacobkl@gmail.com>
2026-03-17 16:01:51 +0530

    postgres serverless neon

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,13 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 
 import * as schema from './schema.ts'
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema })
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is undefined. If you are on Cloudflare Workers, make sure to add it via 'npx wrangler secret put DATABASE_URL'");
+}
+
+const sql = neon(databaseUrl);
+export const db = drizzle(sql, { schema });
